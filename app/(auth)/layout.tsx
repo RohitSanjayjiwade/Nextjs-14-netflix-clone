4f31ac5e35e75107f182e9f6d7c8c2fa80479521
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-10 22:07:03 +0530

    Update AuthLayout component with background image and logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,6 @@
-
+import Image from "next/image";
+import BackgroundImage from '../../public/login_background.jpg'
+import Logo from '../../public/netflix_logo.svg'
 
 export default function AuthLayout({ children }
 	: {
@@ -6,11 +8,26 @@ export default function AuthLayout({ children }
 	}) {
 	return (
 		<div className="relative flex h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+			
+			<Image
+				src={BackgroundImage}
+				alt="background image"
+				className="hidden sm:flex sm:object-cover -z-10 brightness-50"
+				priority
+				fill
+			/>
+
+			<Image 
+			src={Logo}
+			alt="Logo" 
+			width={120}
+			height={120}
+			className="absolute left-4 top-4 object-contain md:left-10 md:top-6 "/>
 			{children}
 		</div>
-		);
+	);
 }
 
 
 
-// border-2 border-gray-300
+// border-2 border-gray-300      
