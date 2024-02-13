bfce9cc5b93babd7c8f847eeb59f120f17e08354
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-13 12:50:27 +0530

    Implement authentication UI components and provider

---
+++ app/components/GoogleSignInButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import GoogleIcon from "../../public/google.svg"
+
+
+
+export default function GoogleSignInButton() {
+	return (
+		<Button variant="outline" size="icon">
+			<Image src={GoogleIcon} alt="Google icon" className="w-6 h-6" />
+		</Button>
+	);
+}
