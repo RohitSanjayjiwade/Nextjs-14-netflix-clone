17abfcb328c5332836f69e378241f246ad323ad2
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-15 12:58:16 +0530

    Add MovieButtons component and expand data selection

--- app/components/MovieVideo.tsx
+++ app/components/MovieVideo.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import prisma from "../utils/db";
+import MovieButtons from "./MovieButtons";
 
 
 async function getData(){
@@ -13,6 +14,7 @@ async function getData(){
 			duration: true,
 			id: true,
 			age: true,
+			youtubeString: true
 		},
 	});
 	return data
@@ -39,8 +41,16 @@ export default async function MovieVideo(){
 			<h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold">{data?.title}</h1>
 			<p className="text-white text-lg mt-5 line-clamp-3">{data?.overview}</p>
 			<div className="flex gap-x-3 mt-4">
-				<Button>See more</Button>
-				<Button>Learn More</Button>
+				<MovieButtons 
+					overview={data?.overview as string} 
+					youtubeUrl={data?.youtubeString as string} 
+					id={data?.id as number} 
+					age={data?.age as number} 
+					title={data?.title as string} 
+					releaseDate={data?.release as number} 
+					duration={data?.duration as number} 
+					key={data?.id}
+				/>
 			</div>
 		</div>
 			
